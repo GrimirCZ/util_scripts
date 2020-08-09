c55d23b42458c1ee37587a865274f4b849af1e51
Vít Falta <falta.vit@zsbcuo.cz>
2020-08-09 22:43:28 +0200

    Added install files for bash and fish shells.

---
+++ install.bash
@@ -0,0 +1,29 @@
+#!/bin/env bash
+
+dst="$HOME/.local/scripts" 
+wd=$(pwd)
+
+if [ "$wd" = "$dst" ]; then
+
+  echo "Skipping creation of $dst and copying scripts to $dst, already at $dst."
+else
+
+  echo "Creating $dst"
+  mkdir -p "$HOME/.local/scripts"
+
+  echo "Copying scripts to $dst"
+  cp -r * "$HOME/.local/scripts"
+  cp -r .[^.]* "$HOME/.local/scripts"
+fi
+
+contains=$(echo $PATH | grep -o "$dst") 
+
+if [ -z "$contains" ]; then
+  echo "Adding $dst to PATH."
+  set -U fish_user_paths $fish_user_paths "$HOME/.local/scripts"
+else 
+  echo "Skipping updating PATH, PATH already contains $dst."
+fi
+
+exit 0
+
---
+++ install.fish
@@ -0,0 +1,29 @@
+#!/bin/env fish
+
+set dst "$HOME/.local/scripts" 
+set wd (pwd)
+
+if test "$wd" = "$dst"
+
+  echo "Skipping creation of $dst and copying scripts to $dst, already at $dst."
+else
+
+  echo "Creating $dst"
+  mkdir -p "$HOME/.local/scripts"
+
+  echo "Copying scripts to $dst"
+  cp -r /etc/skel/* /home/user
+  cp -r /etc/skel/.[^.]* /home/user
+end
+
+set contains (echo $PATH | grep -o "$dst") 
+
+if test -z "$contains"
+  echo "Adding $dst to PATH."
+  set -U fish_user_paths $fish_user_paths "$HOME/.local/scripts"
+else 
+  echo "Skipping updating PATH, PATH already contains $dst."
+end
+
+exit 0
+
